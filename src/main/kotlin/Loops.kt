be349372ef58563419e1d8cb52a105fc639926ab
MeghanaYerninti <meghana.yerninti@sailssoftware.com>
2025-11-20 10:52:40 +0530

    All loops

--- src/main/kotlin/Loops.kt
+++ src/main/kotlin/Loops.kt
@@ -1,5 +1,79 @@
 fun loops() {
 
+//        for loop
+
+    // Loop through a range
+    for (i in 1..5) {
+        println(i)
+    }
+
+    // Loop with step
+    for (i in 1..10 step 2) {
+        println(i)
+    }
+
+    // Reverse loop
+    for (i in 10 downTo 1) {
+        println(i)
+    }
+
+    // Loop through a list
+    val names = listOf("Meghana", "Abhi", "Riya")
+
+    for (name in names) {
+        println(name)
+    }
+
+    // Loop with index
+    for ((index, value) in names.withIndex()) {
+        println("$index → $value")
+    }
+
+//    while loop
+
+    // Basic while
+    var i1 = 1
+    while (i1 <= 5) {
+        println(i1)
+        i1++
+    }
+
+    // Infinite loop (use break)
+    while (true) {
+        println("Running...")
+        break
+    }
+
+//    do while loop
+
+    // basic
+    var i2 = 1
+    do {
+        println(i2)
+        i2++
+    } while (i2 <= 5)
+
+//    for each
+
+    // simple
+    names.forEach {
+        println(it)
+    }
+
+    // with index
+    names.forEachIndexed { index, value ->
+        println("$index → $value")
+    }
+
+//    Loops with Conditions (when + loops)
+    for (i in 1..10) {
+        when {
+            i % 2 == 0 -> println("$i is even")
+            else -> println("$i is odd")
+        }
+    }
+
+
     println("How many numbers will you enter?")
     val amountOfNumbers = readln().toIntOrNull() ?: 0
     var sum = 0
